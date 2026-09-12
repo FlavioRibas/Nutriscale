96dbac41457a35f7ac4383e05ea2f8b70962b4db
FlavioRibas <flavio.crl@gmail.com>
2026-09-12 15:43:38 -0400

    Export recipe migration and sync services

--- src/cloud/index.js
+++ src/cloud/index.js
@@ -1,2 +1,5 @@
 export {getSupabase,isSupabaseConfigured} from './supabaseClient.js';
 export {getCurrentSession,sendMagicLink,signOut,onAuthStateChange} from './auth.js';
+export {loadRecipeCatalogue,loadTags,saveRecipeToCloud,importLocalRecipes,reconcileOwnedRecipes} from './recipes.js';
+export {findLocalPrototypeRecipes,getRecipeImportDecision,recordRecipeImportDecision,importPrototypeRecipes,hydrateRecipeCache} from './migration.js';
+export {refreshRecipesFromCloud,queueRecipeSync,syncRecipesNow} from './recipeSync.js';
